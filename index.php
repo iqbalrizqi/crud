7f0b43efa28833b8572e08c4cacd89fb9f577fc4
iqbalrizqi <iqbalrizqipurnama@gmail.com>
2017-09-22 13:52:52 +0700

    First commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Head</title>
+</head>
+<body>
+<center>
+<h2>Data mahasiswa</h2>
+<table border="1"> 
+	<tr>
+		<td>No</td>
+		<td>Nama</td>
+		<td>Jurusan</td>
+		<td>Alamat</td>
+	</tr>
+	<tr>
+		<td>1</td>
+		<td>Iqbal</td>
+		<td>Sistem Informasi</td>
+		<td>Ciamis</td>
+	</tr>
+</table>
+</center>
+</body>
+</html>
